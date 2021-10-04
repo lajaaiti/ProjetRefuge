6c896ad4022849ba6b4cb0ccacb52dbab41fef8b
anouar.lajaaiti <lajaaiti.anouar@gmail.com>
2021-10-04 16:28:13 +0200

    mise en place des xml

--- app/src/main/java/fr/sam/projetrefuge/MainActivity.java
+++ app/src/main/java/fr/sam/projetrefuge/MainActivity.java
@@ -1,14 +1,44 @@
 package fr.sam.projetrefuge;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    CardView connexion, inscription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        connexion = (CardView) findViewById(R.id.btn_connexion);
+
+        inscription = (CardView) findViewById(R.id.btn_inscription);
+
+        inscription.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, Inscription.class);
+                startActivity(intent);
+            }
+
+        });
+
+       connexion.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent intent = new Intent(getApplicationContext(), Connexion.class);
+               startActivity(intent);
+
+           }
+       });
+
     }
 }

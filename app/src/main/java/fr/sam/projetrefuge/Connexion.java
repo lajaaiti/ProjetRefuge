aaafab5f9ec2367255830ef077ac2cacde1b024b
anouar.lajaaiti <lajaaiti.anouar@gmail.com>
2021-10-06 20:48:20 +0200

    mise en place des activity_mot_de_pass_oublier.xml

--- app/src/main/java/fr/sam/projetrefuge/Connexion.java
+++ app/src/main/java/fr/sam/projetrefuge/Connexion.java
@@ -1,14 +1,32 @@
 package fr.sam.projetrefuge;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Connexion extends AppCompatActivity {
+
+    TextView MDP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connexion);
+
+        MDP =  findViewById(R.id.mot_de_passe_oublier);
+
+        MDP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Connexion.this, MotDePassOublier.class);
+                startActivity(intent);
+            }
+
+        });
     }
 }
---
+++ app/src/main/java/fr/sam/projetrefuge/MotDePassOublier.java
@@ -0,0 +1,14 @@
+package fr.sam.projetrefuge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MotDePassOublier extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mot_de_pass_oublier);
+    }
+}

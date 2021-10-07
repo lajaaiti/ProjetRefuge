4a8ce0b3e00e1965652e9bf704c7e2f9b6c705de
anouar.lajaaiti <lajaaiti.anouar@gmail.com>
2021-10-07 18:49:20 +0200

    commit jojo

--- app/src/main/java/fr/sam/projetrefuge/Profil.java
+++ app/src/main/java/fr/sam/projetrefuge/Profil.java
@@ -3,12 +3,26 @@ package fr.sam.projetrefuge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Profil extends AppCompatActivity {
+
+    Button modifier;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+
+        modifier = (Button) findViewById(R.id.btn_modifier_profil)
+
+                modifier.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(Profil.this, Profil_bis.class);
+                        startActivity(intent);
+                    }
+                });
     }
 }
---
+++ app/src/main/java/fr/sam/projetrefuge/Profil_bis.java
@@ -0,0 +1,14 @@
+package fr.sam.projetrefuge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Profil_bis extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil_bis);
+    }
+}
